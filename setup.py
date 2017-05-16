6a0daad711abaf21acb3f7538d4a01fb1c83e13b
rachmadaniHaryono <foreturiga@gmail.com>
2017-05-16 10:34:40 +0800

    fix: flake8

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
 
     entry_points={
         'console_scripts': [
-              'google-images-download = google_images_download.__main__:cli'
+            'google-images-download = google_images_download.__main__:cli'
         ]
     },
 )
