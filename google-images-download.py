d095a1ae6c1af32a133ac8e6f8611c13834e363e
Hardik Vasa <harniva@amazon.com>
2017-07-02 19:43:48 -0700

    timeout to not wait infinitely to download

--- google-images-download.py
+++ google-images-download.py
@@ -18,7 +18,7 @@ import urllib2
 ########### Edit From Here ###########
 
 #This list is used to search keywords. You can edit this list to search for google images of your choice. You can simply add and remove elements of the list.
-search_keyword = ['Pyramid of Giza','Taj Mahal']
+search_keyword = ['Australia']
 
 #This list is used to further add suffix to your search term. Each element of the list will help you download 100 images. First element is blank which denotes that no suffix is added to the search keyword of the above list. You can edit the list by adding/deleting elements from it.So if the first element of the search_keyword is 'Australia' and the second element of keywords is 'high resolution', then it will search for 'Australia High Resolution'
 keywords = [' high resolution']
@@ -141,7 +141,7 @@ while i<len(search_keyword):
 
         try:
             req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
-            response = urlopen(req)
+            response = urlopen(req,None,15)
             output_file = open(search_keywords+"/"+str(k+1)+".jpg",'wb')
             
             data = response.read()
