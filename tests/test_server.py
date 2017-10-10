a52eef86e24def62937a4db341799ab33e1ad0ca
rachmadaniHaryono <foreturiga@gmail.com>
2017-10-10 21:43:39 +0800

    new: test: simple test for server module

---
+++ tests/test_server.py
@@ -0,0 +1,62 @@
+"""Test server."""
+import logging
+import os
+import tempfile
+import unittest
+
+import pytest
+import vcr
+
+from google_images_download import server
+from google_images_download import models
+
+
+log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+logging.basicConfig(level=logging.DEBUG)
+vcr_log = logging.getLogger("vcr")  # pylint: disable=invalid-name
+vcr_log.setLevel(logging.INFO)
+
+
+class ServerTestCase(unittest.TestCase):
+    """Server test case."""
+
+    def setUp(self):
+        self.db_fd, server.app.config['DATABASE'] = tempfile.mkstemp()
+        server.app.config['SQLALCHEMY_DATABASE_URI'] = \
+            'sqlite:///:memory:'
+        server.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = \
+            False
+        server.app.testing = True
+        self.app = server.app.test_client()
+        # setting app config to suppres warning
+        with server.app.app_context():
+            self.app.application.config.setdefault('WTF_CSRF_ENABLED', False)
+            models.db.init_app(self.app.application)
+            models.db.create_all()
+            try:
+                server.Bootstrap(self.app.application)
+            except AssertionError as err:
+                log.debug('Expected error: %s', err)
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(server.app.config['DATABASE'])
+
+    def test_index(self):
+        """Test index."""
+        rv = self.app.get('/')
+        assert rv.status_code == 200
+        assert rv.data.decode()
+
+    @pytest.mark.no_travis
+    @vcr.use_cassette(record_mode='new_episodes')
+    def test_query(self):
+        """Test method."""
+        with self.app.application.app_context():
+            sq_m, sq_m_created = server.get_or_create_search_query('red', 1)
+            assert sq_m
+            assert sq_m_created
+
+
+if __name__ == '__main__':
+    unittest.main()
