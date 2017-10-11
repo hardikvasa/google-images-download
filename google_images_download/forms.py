7efe65825790c611b9e1d9ba02bdb9c407ae3d63
rachmadaniHaryono <foreturiga@gmail.com>
2017-10-12 07:03:02 +0800

    new: dev: new field on IndexForm

--- google_images_download/forms.py
+++ google_images_download/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Optional, ValidationError
+
+
+def validate_limit(_, field):
+    """Check limit."""
+    if not field.data > 0:
+        raise ValidationError('Limit must be bigger than 0')
 
 
 class IndexForm(FlaskForm):  # pylint: disable=too-few-public-methods
     """Form for index."""
     query = StringField('query', validators=[DataRequired()])
+    disable_image = BooleanField(validators=[Optional()])
+    limit = IntegerField(validators=[Optional(), validate_limit])
