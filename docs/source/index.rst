ab2a7a21fd342dd814a2f0d523441af8d7b1c90f
sujan poudel <spoudel347@gmail.com>
2018-02-24 19:00:36 +0545

    boilerplate setup

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Welcome to google-images-download's documentation!
+=========================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '1.0.0'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='google-images-download',
+    version=__version__,
+    description='Python Script to download hundreds of images from 'Google Images'. It is a ready-to-run code! ',
+    long_description=long_description,
+    url='https://github.com/hardikvasa/google-images-download',
+    download_url='https://github.com/hardikvasa/google-images-download/tarball/' + __version__,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Hardik Vasa',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='hnvasa@gmail.com'
+)
