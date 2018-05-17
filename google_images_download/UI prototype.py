88bb5a7bb7e14768e95bdcea9651edca80a0bd8c
Abdurrahman Rajab <rajab-2@windowslive.com>
2018-05-17 14:57:59 +0300

    change py to python

--- google_images_download/UI prototype.py
+++ google_images_download/UI prototype.py
@@ -71,9 +71,9 @@ text2.pack()
 def  fetch():#ikinci fonksiyon  bir harf yazmak icin  
     
     if path.get() != "":
-        pythonCode =subprocess.Popen("py google_images_download.py  -k \"" + keywords.get() +"\" -o "+ path.get() ,  shell=True ) #burada value alip  gonderiyor 
+        pythonCode =subprocess.Popen("python google_images_download.py  -k \"" + keywords.get() +"\" -o "+ path.get() ,  shell=True ) #burada value alip  gonderiyor 
     else :
-        pythonCode =subprocess.Popen("py google_images_download.py  -k\"" + keywords.get()+"\"" ,shell=True)
+        pythonCode =subprocess.Popen("python google_images_download.py  -k\"" + keywords.get()+"\"" ,shell=True)
     
  
 
