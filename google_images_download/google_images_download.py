d05d0614e6843c7467d36a41630f02e2ce9ce339
neon_cyan <neon-cyan@users.noreply.github.com>
2018-08-11 21:03:29 +0100

    Added a sample config file and made metadata export to a json (#132)
    * Added sample config & export to json instead of txt
    
    * Fixed typo
    
    * Added logs to .gitignore

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -843,16 +843,16 @@ class googleimagesdownload:
                     items,errorCount,abs_path = self._get_all_items(raw_html,main_directory,dir_name,limit,arguments)    #get all image items and download images
                     paths[pky + search_keyword[i] + sky] = abs_path
 
-                    #dumps into a text file
+                    #dumps into a json file
                     if arguments['extract_metadata']:
                         try:
                             if not os.path.exists("logs"):
                                 os.makedirs("logs")
                         except OSError as e:
                             print(e)
-                        text_file = open("logs/"+search_keyword[i]+".txt", "w")
-                        text_file.write(json.dumps(items, indent=4, sort_keys=True))
-                        text_file.close()
+                        json_file = open("logs/"+search_keyword[i]+".json", "w")
+                        json.dump(items, json_file, indent=4, sort_keys=True)
+                        json_file.close()
 
                     #Related images
                     if arguments['related_images']:
