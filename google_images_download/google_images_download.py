86d8282939dd673b700a358f22cbf9b52ef6b557
Logan Castrucci <logancastrucci@gmail.com>
2018-11-07 21:48:21 -0500

    url

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -562,7 +562,7 @@ class googleimagesdownload:
         if print_urls or no_download:
             print("Image URL: " + image_url)
         if no_download:
-            return "success","Printed url without downloading",None,None
+            return "success","Printed url without downloading",None,image_url
         try:
             req = Request(image_url, headers={
                 "User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
