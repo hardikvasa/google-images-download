f694cce7f54943d4d15cf9900f6d3148b0c4ed31
Hardik Vasa <hnvasa@gmail.com>
2019-05-11 21:48:46 -0700

    minor doc CSS update

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ html_static_path = ['_static']
 
 html_context = {
     'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
+        '_static/overrides.css',  # override wide tables in RTD theme
         ],
      }
 
