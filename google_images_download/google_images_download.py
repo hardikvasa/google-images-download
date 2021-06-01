22bb005827ba9aaaf57c093d7e31c07f478b09bf
lilylovecode123 <84236799+lilylovecode123@users.noreply.github.com>
2021-06-01 22:12:31 +0800

    Update google_images_download.py
    convert png to jpg

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -181,6 +181,30 @@ class googleimagesdownload:
                 # Move files of the same format to the corresponding format folder
                 shutil.move(file, filetype)
                 COUNT += 1
+                
+                
+      # convert png to jpg
+    def PNG_JPG(PngPath):
+        img = cv.imread(PngPath, 0)
+        w, h = img.shape[::-1]
+        infile = PngPath
+        outfile = os.path.splitext(infile)[0] + ".jpg"
+        img = Image.open(infile)
+        img = img.resize((int(w / 2), int(h / 2)), Image.ANTIALIAS)
+        try:
+            if len(img.split()) == 4:
+                # prevent IOError: cannot write mode RGBA as BMP
+                r, g, b, a = img.split()
+                img = Image.merge("RGB", (r, g, b))
+                img.convert('RGB').save(outfile, quality=70)
+                os.remove(PngPath)
+            else:
+                img.convert('RGB').save(outfile, quality=70)
+                os.remove(PngPath)
+            return outfile
+        except Exception as e:
+            print("Error convert png to jpg", e)
+
             
             
 
